5f5ca1c38d04372bf5aef3b4af977652e4f659a2
Samandar Shoyimov <90138640+Samandar003@users.noreply.github.com>
2022-03-31 12:00:54 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-3mijgb#=v&5&tcmy@umd3$+0n9v*osgjm9)xd1y=b4g7tf7w^w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','blog-samandar.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
